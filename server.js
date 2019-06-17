2bb77f154b431214a9cdf141cdf5fe25d631d0de
alvenmaretinus <alven.maretinus@tokopedia.com>
2019-06-17 11:01:54 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 9000;
+
+require('dotenv').config();
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+mongoose.connect(
+  process.env.DB_CLUSTER_URL,
+  { useNewUrlParser: true },
+  () => {
+    console.log('Connected to MongoDB Atlas')
+  }
+)
+
+app.listen(port, () => {
+  console.log('API server is running')
+})
